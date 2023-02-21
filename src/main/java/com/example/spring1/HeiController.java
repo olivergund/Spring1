d147318b7e8470b54f99ea90f6af154b010beaef
olivergund <olivergundrosen@outlook.com>
2023-02-21 17:43:31 +0100

    Initial commit

---
+++ src/main/java/com/example/spring1/HeiController.java
@@ -0,0 +1,13 @@
+package com.example.spring1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeiController {
+    @GetMapping("/")
+    public String hei(String navn){
+        int antallBokstaver = navn.length();
+        return "Hei verden " + navn + "! Navnet ditt er " + antallBokstaver + " bokstaver langt";
+    }
+}
